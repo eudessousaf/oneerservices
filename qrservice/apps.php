2d00cbdaebb8c3dece04e635f49146c52e8d88ee
eudes sousa <eudessousa@MacBook-Air-de-eudes.local>
2025-04-05 01:37:57 -0300

    edit app default > ifood

--- qrservice/apps.php
+++ qrservice/apps.php
@@ -1,10 +1,10 @@
 <?php
 return [
     'default' => [
-        'android' => 'https://www.instagram.com/android/',
-        'ios' => 'https://www.instagram.com/apple/',
+        'android' => 'https://play.google.com/store/apps/details?id=br.com.brainweb.ifood&pcampaignid=web_share',
+        'ios' => 'https://apps.apple.com/br/app/ifood-pedir-delivery-em-casa/id483017239',
     ],
-    'zeapp' => [
+    'app01' => [
         'android' => 'https://play.google.com/store/apps/details?id=com.exemplo.zeapp',
         'ios' => 'https://apps.apple.com/app/id2222222222',
     ],
