d6f3f1c5e4d478690b5e033316c421aeb9f4755b
eudes sousa <eudessousa@MacBook-Air-de-eudes.local>
2025-04-05 14:27:35 -0300

    add qr service rota66car

--- qrservice/apps.php
+++ qrservice/apps.php
@@ -4,8 +4,8 @@ return [
         'android' => 'https://play.google.com/store/apps/details?id=br.com.brainweb.ifood&pcampaignid=web_share',
         'ios' => 'https://apps.apple.com/br/app/ifood-pedir-delivery-em-casa/id483017239',
     ],
-    'app01' => [
-        'android' => 'https://play.google.com/store/apps/details?id=com.exemplo.zeapp',
-        'ios' => 'https://apps.apple.com/app/id2222222222',
+    'rota66car' => [
+        'android' => 'https://play.google.com/store/apps/details?id=br.com.client.rota66car&pcampaignid=web_share',
+        'ios' => 'https://apps.apple.com/br/app/rota-66-car-passageiro/id6743943930',
     ],
 ];
